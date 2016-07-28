42b1202a05fdb26614fb99ba8f36c71e9d6b4bda
dvkndn <dvkndn@gmail.com>
2016-07-29 03:02:09 +0700

    build dist

--- dist/en.js
+++ dist/en.js
@@ -229,7 +229,7 @@ exports['default'] = {
   'volume': {
     'title': 'Transacted Volumes',
     'email-report': 'Email Report',
-    'email-report-choose-range-first': 'You need to choose "From" and "To" for the email report',
+    'email-report-choose-range-first': 'You need to choose "From" and "To" date for the email report.',
     'from': 'From',
     'to': 'To'
   },
